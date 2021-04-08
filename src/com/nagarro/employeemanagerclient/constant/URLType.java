dd1648c7d3b356285c1742e1c9937f335c8a4b3e
SHARUN-KANOJIA <55599750+SHARUN-KANOJIA@users.noreply.github.com>
2021-04-08 22:47:19 +0530

    Add files via upload

---
+++ src/com/nagarro/employeemanagerclient/constant/URLType.java
@@ -0,0 +1,11 @@
+package com.nagarro.employeemanagerclient.constant;
+
+public class URLType {
+
+	public static final String GET_EMPS_URL = "http://localhost:8082/EmpmanApi/webapi/employees";
+	public static final String GET_EMP_URL = "http://localhost:8082/EmpmanApi/webapi/employees/employee/";
+	public static final String UPLD_EMP_URL = "http://localhost:8082/EmpmanApi/webapi/employees/employee";
+	public static final String UPDT_EMP_URL = "http://localhost:8082/EmpmanApi/webapi/employees/employee";
+	public static final String DLT_EMP_URL = "http://localhost:8082/EmpmanApi/webapi/employees/employee/";
+
+}
